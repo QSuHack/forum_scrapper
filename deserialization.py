ddee65fce6aa7b27d79f46d692011d63c9e19cac
QSuhack <graczgge@wp.pl>
2020-07-18 19:48:22 +0200

    Some refactor

--- deserialization.py
+++ deserialization.py
@@ -4,8 +4,9 @@ with open("backup", mode="rb") as backup:
     forum_from_backup = _pb2.Forum()
     forum_from_backup.ParseFromString(backup.read())
     print(forum_from_backup)
-    for s in forum_from_backup.Sb:
-        for t in s.theards:
-            print(t.title)
+    for subforum in forum_from_backup.Sb:
+        for theard in subforum.theards:
+            print(theard.title)
+            # Here next 'for' for post and you can get each property of post and use it, print or write it.
     with open("backup.txt", mode="w+", encoding="utf-8") as b:
-        b.write(str(forum_from_backup))
+        b.write(str(forum_from_backup)) # Not writing properly Polish characters, but it can be fixed by long version above.
